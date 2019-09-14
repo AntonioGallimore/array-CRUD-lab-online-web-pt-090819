62cc5649cad67f92eaa12e9d00a903811ae2ba90
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-09-14 01:31:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,11 +22,13 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  numbers = ["wow", 1, 2, 3 ,4]
+  numbers.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow", "I", "am", "arrays!"]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
